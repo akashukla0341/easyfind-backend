9a2311d508ede9d8b43e48574f885474ac08b323
Akash Shukla <akashukla0341@gmail.com>
2024-06-16 21:39:39 +0530

    changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const connectDB = require('./config/db')
@@ -7,6 +8,7 @@ const router = require('./routes')
 
 
 const app = express()
+const PORT = 8080 || process.env.PORT
 app.use(cors({
     origin : process.env.FRONTEND_URL,
     credentials : true
@@ -16,8 +18,8 @@ app.use(cookieParser())
 
 app.use("/api",router)
 
-const PORT = 8080 || process.env.PORT
 
+app.use('/api/webhook', bodyParser.raw({ type: 'application/json' }));
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
